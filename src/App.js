5ec3295507c90be6e9b9b4382bfc128f9ebcc422
user <sivtsovatanya1985@gmail.com>
2022-05-02 16:50:45 +0300

    - added filter

--- src/App.js
+++ src/App.js
@@ -12,10 +12,11 @@ class App extends Component {
         this.state = {
             data: [
                 {name: 'John C.', salary: 800, increase: false, rise: false, id: 1},
-                {name: 'Alex M.', salary: 3000, increase: true, rise: false,id: 2},
+                {name: 'Alex M.', salary: 3000, increase: true, rise: true,id: 2},
                 {name: 'Carl W.', salary: 5000, increase: false, rise: false,id: 3}
             ],
-            term: ''
+            term: '',
+            filter: 'all'
         }
         this.maxId = 4;
     }
@@ -102,17 +103,34 @@ class App extends Component {
     onUpdateSearch = (term) => {
         this.setState({term: term})
     }
+
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
+
     render() {
-        const {data, term} = this.state;
+        const {data, term, filter} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
-        const visibleData = this.searchEmp(data,term);
+        const visibleData = this.filterPost(this.searchEmp(data,term), filter);
         return (
             <div className='app'>
                 <AppInfo employees={employees} increased={increased} />
                 <div className='search-panel'>
                     <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
-                    <AppFilter/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
                 <EmployeesList
                     data={visibleData}
